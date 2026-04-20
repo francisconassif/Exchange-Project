7de86b7d3cf5ce48dc9559a515017b942db0449e
francisconassif <francisconassif270902@gmail.com>
2026-04-20 02:38:14 -0600

    updated backend april 20th

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,15 +1,8 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'http://localhost:3000',
-  'http://localhost:5432',
   'http://localhost:5173',
   'https://jorgenavarro13.github.io'
-  // // 'https://jorgenavarro13.github.io/rockwell_ot_cybersecurity_frontend/',
-  // // 'https://jorgenavarro13.github.io/rockwell_ot_cybersecurity_frontend/#'
-  // // 'https://jorgenavarro13.github.io/rockwell_ot_cybersecurity_frontend/#/'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
